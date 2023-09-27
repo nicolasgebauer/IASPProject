db1dcb2ed42818c74334c9d81050eed71e3dbc06
nicolasgebauer <ngebauer@miuandes.cl>
2023-09-27 17:36:48 -0300

    Agragacion de mas modelos

--- HopeSolutions/Core/models.py
+++ HopeSolutions/Core/models.py
@@ -32,12 +32,35 @@ class Warehouse(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     type = models.IntegerField()
-    bsale = models.IntegerField()
+    integration_id = models.IntegerField()
     sync = models.IntegerField()
     removed = models.IntegerField()
     supplied = models.ForeignKey(
         "self", null=True, blank=True, on_delete=models.CASCADE
     )
+
+    def __str__(self):
+        return self.name
+
+#INVENTARIO
+class Inventory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
+    stock = models.IntegerField()
+    removed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product.name
+    
+#INTEGRACION (APIs que debe consumir el negocio, En este caso APIs ficticias)
+class Integration(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    type = models.IntegerField()
+    url = models.CharField(max_length=255)
+    token = models.CharField(max_length=255)
+    removed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
