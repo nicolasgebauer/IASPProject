ddddf4b1bb8d035428eaa5844e3fb02448a171d8
nicolasgebauer <ngebauer@miuandes.cl>
2023-09-28 01:24:48 -0300

    seed data de testeo

--- HopeSolutions/Core/models.py
+++ HopeSolutions/Core/models.py
@@ -13,21 +13,19 @@ class Product(models.Model):
     category = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     cost = models.DecimalField(max_digits=10, decimal_places=2)
+    size = models.CharField(max_length=255, null=True, blank=True)
     category2 = models.CharField(max_length=255, null=True, blank=True)
     gender = models.CharField(max_length=255, null=True, blank=True)
     creationDate = models.DateTimeField()
     removed = models.BooleanField(default=False)
     inventoryUpdate = models.IntegerField(null=True, blank=True)
-    inventoryUpdateDate = models.DateTimeField(null=True, blank=True)
-    inventoryUpdateOrigin = models.CharField(max_length=255, null=True, blank=True)
-    inventoryUpdateOriginDate = models.DateTimeField(null=True, blank=True)
-    
+    inventoryUpdateDate = models.DateTimeField(null=True, blank=True)    
 
     def __str__(self):
         return self.name
     
 
-#BODEGA
+#BODEGA (o tienda fisica)
 class Warehouse(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
