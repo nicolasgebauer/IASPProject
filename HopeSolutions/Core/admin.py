eb191f2a6bdf46df917c3e488ec818b56e57dc28
nicolasgebauer <ngebauer@miuandes.cl>
2023-09-27 23:46:18 -0300

    clases listas

--- HopeSolutions/Core/admin.py
+++ HopeSolutions/Core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Product)
+admin.site.register(Warehouse)
+admin.site.register(Inventory)
+admin.site.register(Integration)
+admin.site.register(Client)
+admin.site.register(Sale)
